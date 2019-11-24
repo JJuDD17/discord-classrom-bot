bfebd6aae65f42899e9a9efd156cb097b63f2730
jjudd17 <jjudd17@mail.ru>
2019-11-24 12:42:57 +0300

    Update database.py

--- database.py
+++ database.py
@@ -40,7 +40,10 @@ async def get_task(task_id, description_only=False):
         result = await conn.fetchrow('SELECT description FROM tasks WHERE id = $1', task_id)
         return result.description
     else:
-        return tuple(await conn.fetchrow('SELECT file, file_name, description FROM tasks WHERE id = $1', task_id))
+        result = await conn.fetchrow('SELECT file, file_name, description FROM tasks WHERE id = $1', task_id)
+        if result:
+            return tuple(result)
+        return None
 
 
 async def add_solution(student_id, task_id, file, file_name):
@@ -50,24 +53,35 @@ async def add_solution(student_id, task_id, file, file_name):
 
 async def get_solution(task_id, student_id):
     global conn
-    return tuple(await conn.fetchrow('SELECT file, file_name FROM solutions WHERE student_id = $1 AND task_id = $2',
-                                     student_id, task_id))
+    result = await conn.fetchrow('SELECT file, file_name FROM solutions WHERE student_id = $1 AND task_id = $2',
+                                     student_id, task_id)
+    if result:
+        return tuple(result)
+    return None
 
 
 async def who_has_solution(task_id):
     global conn
-    return [i.student_id for i in await conn.fetch('SELECT student_id FROM solutions WHERE task_id = $1', task_id)]
+    result = await conn.fetch('SELECT student_id FROM solutions WHERE task_id = $1', task_id)
+    if result:
+        return [i.student_id for i in result]
+    return None
 
 
 async def tasks_done(student_id):
     global conn
-    return [i.task_id for i in
-            await conn.fetch('SELECT task_id FROM solutions WHERE student_id = $1', student_id)]
+    result = await conn.fetch('SELECT task_id FROM solutions WHERE student_id = $1', student_id)
+    if result:
+        return [i.task_id for i in result]
+    return None
 
 
 async def all_task_ids():
     global conn
-    return [i.id for i in await conn.fetch('SELECT id FROM tasks')]
+    result = await conn.fetch('SELECT id FROM tasks')
+    if result:
+        return [i.id for i in result]
+    return None
 
 
 async def add_mark(mark, task_id, student_id):
@@ -77,12 +91,14 @@ async def add_mark(mark, task_id, student_id):
 
 async def get_mark(task_id, student_id):
     global conn
-    result = await conn.fetchval('SELECT mark FROM marks WHERE task_id = $1 AND student_id = $2', task_id, student_id)
-    return result
+    return await conn.fetchval('SELECT mark FROM marks WHERE task_id = $1 AND student_id = $2', task_id, student_id)
 
 
 async def get_marks(student_id):
-    return [i.mark for i in await conn.fetch('SELECT mark FROM marks WHERE student_id = $1', student_id)]
+    result = await conn.fetch('SELECT mark FROM marks WHERE student_id = $1', student_id)
+    if result:
+        return [i.mark for i in result]
+    return None
 
 
 async def close():
